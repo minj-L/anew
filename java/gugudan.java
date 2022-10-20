2e3d9a1f1569c2c5a2f0e63f9fc83ba54970f6d2
minj-L <52521457+minj-L@users.noreply.github.com>
2022-10-20 17:35:43 +0900

    javac실기 구구단 출력

---
+++ java/gugudan.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class gugudan {
+  public static void main(String[] args){
+    int num;
+    Scanner sc = new Scanner(System.in);
+    while(true){
+      System.out.print("출력할 구구단을 선택하세요 : ");
+      num = sc.nextInt();
+      if (num < 1 && num > 10){
+        System.out.print("해당 숫자는 구구단을 출력할 수 없습니다 다시 입력하세요");
+      } else if (num == 0 ){
+        System.out.print(0);
+      }else{
+        for (int j = 1; j <= 9; j++){
+          System.out.println(num + " * " + j + "=" + num * j);
+        }
+      }
+    }
+  }
+}
